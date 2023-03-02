8acbff7f652b78512f9434d790781788387e799d
Vagif Kantemirov <120570846+kntmrv@users.noreply.github.com>
2023-03-02 19:32:17 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -48,3 +48,20 @@ if (ScrollTrigger.isTouch !== 1) {
 	})
 
 }
+// Audio
+
+let soundButton = document.querySelector('.soundbutton'),
+		audio = document.querySelector('.audio')
+
+soundButton.addEventListener('click', e => {
+	soundButton.classList.toggle('paused')
+	audio.paused ? audio.play() : audio.pause()
+})
+
+window.onfocus = function() {
+	soundButton.classList.contains('paused') ? audio.pause() : audio.play()
+}
+
+window.onblur = function() {
+	audio.pause()
+}
